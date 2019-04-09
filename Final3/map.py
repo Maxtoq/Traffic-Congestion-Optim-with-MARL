49b1d56e2d8ea41868f234b23eafa92516dd86ec
LAPTOP-D4MUNBTJ\Simon <simon.primeau@edu.ece.fr>
2019-04-09 14:06:04 +0200

    Update

--- Final3/map.py
+++ Final3/map.py
@@ -5,6 +5,7 @@ from random import randint
 
 import constants as tc
 import numpy as np
+from Agent import Agent
 
 """Quelques variable globales pour le fun"""
 junctionID = 'gneJ33' #pour le module de stats
@@ -31,142 +32,7 @@ def get_options():
     options, args = opt_parser.parse_args()
     return options
 	
-class Information:
-	def _init_(self):
-		self.calculatedWaitingTime = -1
-		self.realWaitingTime = -1
-		self.calculatedDistanceDriven = -1
-		self.realDistanceDriven= -1
-		self.sigma = -1
-		
-	def _init_(self, WT, DD, sigma):
-		self.calculatedWaitingTime = WT
-		self.realWaitingTime = -1
-		self.calculatedDistanceDriven = DD
-		self.realDistanceDriven= -1
-		self.sigma = sigma
-		
-	def get_CWT():
-		return self.calculatedWaitingTime
-	def get_RWT():
-		return self.realWaitingTime
-	def get_CDD():
-		return self.calculatedDistanceDriven
-	def get_RDD():
-		return self.realDistanceDriven
-	def get_sigma():
-		return self.sigma
-		
-	def set_CWT(self, p):
-		self.calculatedWaitingTime = p
-	def set_RWT(self, p):
-		self.realWaitingTime = p
-	def set_CDD(self, p):
-		self.calculatedDistanceDriven = p
-	def set_RDD(self, p):
-		self.realDistanceDriven = p
-	def set_sigma(self, p):
-		self.sigma = p
-		
-		
-class Map:
-	def __init__(self):
-		self.Graph = traci.edge.getIDList()
-		self.Ponderation = np.empty(len(self.Graph), dtype=int)#traci.edge.getIDList()
-		for i in range(len(self.Ponderation)):
-			self.Ponderation[i]= 0	
-		
-		
-	def set_Pnd_At(self, id, value):
-		self.Ponderation[id] = value
-	def get_Pnd_At(self, id):
-		return self.Ponderation[id]
-	def get_Size(self):
-		return len(self.Graph)
-	def get_Pnd_At_Edge(self, name):
-		return self.Ponderation[self.Graph.index(name)]
-		
 
-class Agent:
-	
-	def __init__(self, id):
-	
-		self.info = Information()
-		self.map = Map()
-	
-		"""Constructeur"""
-		self.id = str(id)
-		self.type = "car"
-
-		"""self.vitesse = 50
-		self.acceleration = 0"""
-		
-		al = 0
-		be = 0
-		 
-		global R
-		
-		while("ON A PAS UNE ROUTE VALIDE"):
-			while(al == be):
-				al = randint(0, traci.edge.getIDCount()-1)
-				be = randint(0, traci.edge.getIDCount()-1)
-				
-				
-			EdgeL = traci.edge.getIDList()
-
-			"""print("EdgeL = " + str(len(EdgeL)))	
-			print("a = " + str(al))
-			print("beta = " + str(be))"""
-			
-			self.pos = EdgeL[int(al)]
-			self.dest = EdgeL[int(be)]
-			
-			self.route = traci.simulation.findRoute(self.pos, self.dest, self.type)
-			self.Nodes = traci.simulation.findRoute(self.pos, self.dest, self.type).edges
-			
-			"""print("START : "+ str(id))
-			print(self.route)
-			print(self.route.edges)
-			print(self.type)"""
-			
-			if(len(self.Nodes)> 4 ):
-				print("out")
-				break
-			else: 
-				al = 0
-				be = 0
-				
-		traci.route.add("rd"+str(R), self.Nodes)
-		traci.vehicle.add(str(self.id), "rd"+str(R), str(self.type)) 
-		
-		#c = np.random.normal(0.5, 0.1)
-		#traci.vehicle.set
-		R += 1
-		
-		
-	#GETTERSs
-	def get_Dest(self):
-		return traci.vehicle.getRoute(str(self.id))
-		
-	def get_Vitesse(self):
-		return traci.vehicle.getRoute(str(self.id))
-		
-	#SETTERS
-	def set_Dest(self, dest):
-		#print(type(traci.simulation.findRoute(traci.vehicle.getRoadID(str(self.id)), dest, "car")))
-		traci.vehicle.setRoute(str(self.id),  traci.simulation.findRoute(traci.vehicle.getRoadID(str(self.id)), dest, "car").edges)
-		
-	def give_pop_at_egde_Oriente(self, name):
-		return getLastStepVehicleNumber(name)
-	
-	def give_pop_at_egde(self, name):
-		opposite = -int(name)
-		
-		opposite = str(opposite)
-		
-		return getLastStepVehicleNumber(name) + getLastStepVehicleNumber(opposite)
-	
-	
 			
 
 def CreateAgents(AgentList):
--- Final3/schem.py
+++ Final3/schem.py
@@ -3,7 +3,7 @@ import pandas as pd
 import xml.etree.cElementTree as et
 parsedXML = et.parse( "data.xml" )
 
-dfcols = ['Id','vType','departLane' ,'arrivalLane', 'rerouteNo','timeLoss', 'stopTime', 'waitingTime', 'routeLength', 'duration']   
+dfcols = ['Id','vType','departLane' ,'arrivalLane','timeLoss', 'waitingTime', 'routeLength', 'duration']   
 df = pd.DataFrame(columns=dfcols)
 
 def getvalueofnode( node ):
@@ -15,13 +15,11 @@ for child in parsedXML.getroot():
 	vType = child.attrib.get('vType')
 	departLane = child.attrib.get('departLane')
 	arrivalLane = child.attrib.get('arrivalLane')
-	rerouteNo = child.attrib.get('rerouteNo')
 	timeLoss = child.attrib.get('timeLoss')
-	stopTime = child.attrib.get('stopTime')
 	waitingTime = child.attrib.get('waitingTime')
 	routeLength = child.attrib.get('routeLength')
 	duration = child.attrib.get('duration')	
-	df = df.append( pd.Series( [Id,vType,departLane,arrivalLane,rerouteNo,timeLoss,stopTime,waitingTime,routeLength,
+	df = df.append( pd.Series( [Id,vType,departLane,arrivalLane,timeLoss,waitingTime,routeLength,
 	duration],index=dfcols) ,ignore_index=True)
 
 df.to_pickle("datastream") 
