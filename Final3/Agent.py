5b32d452eb8e2dbc8da1accefd966b7f8275279f
Maxtoq <maxime.toquebiau@hotmail.fr>
2019-04-09 15:58:49 +0200

    use data_parsed

--- Final3/Agent.py
+++ Final3/Agent.py
@@ -46,57 +46,61 @@ class Agent:
 		self.id = str(id)
 		self.type = "car"
 		
-		al = 0
-		be = 0
-		
-		while("ON A PAS UNE ROUTE VALIDE"):
-			while(al == be):
-				al = randint(0, traci.edge.getIDCount()-1)
-				be = randint(0, traci.edge.getIDCount()-1)
-				
-			EdgeL = traci.edge.getIDList()
-
-			"""print("EdgeL = " + str(len(EdgeL)))	
-			print("a = " + str(al))
-			print("beta = " + str(be))"""
-			
-			self.pos = EdgeL[int(al)]
-			self.dest = EdgeL[int(be)]
-			
-			self.route = traci.simulation.findRoute(self.pos, self.dest, self.type)
-			self.Nodes = traci.simulation.findRoute(self.pos, self.dest, self.type).edges
-			
-			"""print("START : "+ str(id))
-			print(self.route)
-			print(self.route.edges)
-			print(self.type)"""
-			
-			if(len(self.Nodes)> 4 ):
-				print("out")
-				break
-			else: 
-				al = 0
-				be = 0
-		
-		traci.route.add("rd" + str(Agent.ROAD_ID), self.Nodes)
-		traci.vehicle.add(str(self.id), "rd"+str(Agent.ROAD_ID), str(self.type))
-		
-		#c = np.random.normal(0.5, 0.1)
-		#traci.vehicle.set
-		Agent.ROAD_ID += 1
+		self.set_random_route()
 		
 		
 	#GETTERSs
-	def get_Dest(self):
-		return traci.vehicle.getRoute(str(self.id))
+	#def get_Dest(self):
+	#	return traci.vehicle.getRoute(str(self.id))
 		
-	def get_Vitesse(self):
-		return traci.vehicle.getRoute(str(self.id))
+	#def get_Vitesse(self):
+	#	return traci.vehicle.getRoute(str(self.id))
 		
 	#SETTERS
 	def set_Dest(self, dest):
 		#print(type(traci.simulation.findRoute(traci.vehicle.getRoadID(str(self.id)), dest, "car")))
 		traci.vehicle.setRoute(str(self.id),  traci.simulation.findRoute(traci.vehicle.getRoadID(str(self.id)), dest, "car").edges)
+
+	def set_random_route(self):
+		start_edge = 0
+		dest_edge = 0
+
+		while("ON A PAS UNE ROUTE VALIDE"):
+			while(start_edge == dest_edge):
+				start_edge = randint(0, traci.edge.getIDCount()-1)
+				dest_edge = randint(0, traci.edge.getIDCount()-1)
+
+			EdgeL = traci.edge.getIDList()
+
+			"""print("EdgeL = " + str(len(EdgeL)))	
+			print("a = " + str(al))
+			print("beta = " + str(be))"""
+
+			self.pos = EdgeL[start_edge]
+			self.dest = EdgeL[dest_edge]
+
+			self.route = traci.simulation.findRoute(self.pos, self.dest, self.type)
+			self.Nodes = traci.simulation.findRoute(
+			    self.pos, self.dest, self.type).edges
+
+			"""print("START : "+ str(id))
+			print(self.route)
+			print(self.route.edges)
+			print(self.type)"""
+
+			if(len(self.Nodes) > 4):
+				print("out")
+				break
+			else:
+				start_edge = 0
+				dest_edge = 0
+
+		traci.route.add("rd" + str(Agent.ROAD_ID), self.Nodes)
+		traci.vehicle.add(str(self.id), "rd"+str(Agent.ROAD_ID), str(self.type))
+
+		#c = np.random.normal(0.5, 0.1)
+		#traci.vehicle.set
+		Agent.ROAD_ID += 1
 		
 	def give_pop_at_egde_Oriente(self, name):
 		return getLastStepVehicleNumber(name)
--- Final3/Data_parsed_files.py
+++ Final3/Data_parsed_files.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import xml.etree.cElementTree as et
 
-
 class Data_parsed_file:
 
-	
-	def __init__ (self,name,id_list):
+	def __init__ (self, name, id_list=None):
 		self.name = name
 		self.id_list = id_list
 		parsedXML = et.parse(name)
-		dfcols = ['Id','vType','departLane' ,'arrivalLane','timeLoss', 'waitingTime', 			'routeLength', 'duration']   
+		dfcols = ['Id','vType','departLane' ,'arrivalLane','timeLoss', 'waitingTime', 'routeLength', 'duration']   
 		df = pd.DataFrame(columns=dfcols)
 		for child in parsedXML.getroot():
 			tripinfo = child.get('tripinfo')
@@ -20,21 +18,20 @@ class Data_parsed_file:
 			timeLoss = child.attrib.get('timeLoss')
 			waitingTime = child.attrib.get('waitingTime')
 			routeLength = child.attrib.get('routeLength')
-			duration = child.attrib.get('duration')	
-			if Id in id_list:				
-				df=df.append( pd.Series( [Id,vType,departLane,arrivalLane,
-					timeLoss,waitingTime,routeLength,
-					duration],index=dfcols) ,ignore_index=True)
+			duration = child.attrib.get('duration')
+
+			if self.id_list is not None:
+				if Id in self.id_list:				
+					df=df.append(pd.Series([Id, vType, departLane, arrivalLane,
+									timeLoss, waitingTime, routeLength,
+									duration], index=dfcols), ignore_index=True)
+			else:
+				df = df.append(pd.Series([Id, vType, departLane, arrivalLane,
+                                    timeLoss, waitingTime, routeLength,
+                                    duration], index=dfcols), ignore_index=True)
 		print(df)
 
+	def getvalueofnode(self, node):
+		return node.text if node is not None else None
 
-
-				
-
-
-	
-	def getvalueofnode( node ):
-		return notde.text if node is not None else None
-
-a = Data_parsed_file("data.xml", [1, 3])
 
