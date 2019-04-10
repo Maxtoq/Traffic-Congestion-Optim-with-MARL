1f15230e6b09380e14e4c9bdb9c9a3e1112dfc5c
Maxtoq <maxime.toquebiau@hotmail.fr>
2019-04-10 16:24:31 +0200

    suite ql

--- Final3/OurAgent.py
+++ Final3/OurAgent.py
@@ -1,3 +1,5 @@
+import traci
+
 from Agent import InterestingAgent
 
 
@@ -9,6 +11,35 @@ class OurAgent(InterestingAgent):
 		# Add access to the QLearning Unit
 		self.qlu = qlu
 
+        # Current state
+        self.curr_state = None
+
+        # Action to take
+        self.action = -1
+
 	def find_state(self):
 		""" Translate data from SUMO to a state usable by the QLU. """
-		return 1
+        # Get the edge where are
+		edge = self.get_Edge_At()
+
+        # Get the population at the next edges
+        pop_next_edges = self.get_pop_next_edges()
+
+        return (edge[0], edge[1], self.end_n, pop_next_edges[0], 
+                           pop_next_edges[1], pop_next_edges[2])
+
+    def get_action(self):
+        """ Get an action from the QLU. """
+        self.action = self.qlu.choose_action(self.curr_state)
+
+    def judge_action(self):
+        """ Get new state and update q value with the Bellman's equation. """
+        # Get the new state
+        new_state = self.find_state()
+
+        # Update the Q value
+        self.qlu.compute_bellman(self.action, self.curr_state, new_state)
+
+        # Update state
+        self.curr_state = new_state
+
--- Final3/QLearningUnit.py
+++ Final3/QLearningUnit.py
@@ -3,6 +3,19 @@ import random as rd
 import warnings
 warnings.filterwarnings('ignore')
 
+
+def discr_pop(pop):
+    """ Discretise a population value. """
+    elif (pop < 11):
+        return 0
+    elif (pop < 21):
+        return 1
+    elif (pop < 36):
+        return 2
+    else:
+        return 3
+
+
 class QLearningUnit:
 
     # STATES
@@ -11,7 +24,7 @@ class QLearningUnit:
     # Number of destination edges
     NB_DEST_EDGE = 3
     # Number of discrete values for the 'number of car in one edge'
-    NB_CARNB_IN_EDGE = 4
+    NB_POP_IN_EDGE = 4
 
     def __init__(self):
         # Q-table, contains all the Q-values for all the (state-action) couples
@@ -23,10 +36,7 @@ class QLearningUnit:
         #         - nb of cars on edge 3
         # Actions: go left, go straight, go right
         self.qtable = np.zeros((self.NB_EDGES * 2 * self.NB_DEST_EDGE 
-                                * (self.NB_CARNB_IN_EDGE ** 3), 3))
-
-        self.actions = []
-        self.rewards = []
+                                * (self.NB_POP_IN_EDGE ** 3), 3))
         
         # Exploration rate
         self.eps = 1
@@ -42,16 +52,16 @@ class QLearningUnit:
     def get_state_id(self, state):
         """ Returns the id of a state in the Q-table. """
         # state = (# of the edge (0-28), orientation (0-1), destination (0-2), nb of cars on edge 1 (0-3), nb of cars on edge 2 (0-3), nb of cars on edge 3 (0-3))
-        return (state[0] * 2 * self.NB_DEST_EDGE * (self.NB_CARNB_IN_EDGE ** 3) 
-                + state[1] * self.NB_DEST_EDGE * (self.NB_CARNB_IN_EDGE ** 3)
-                + state[2] * (self.NB_CARNB_IN_EDGE ** 3) 
-                + state[3] * (self.NB_CARNB_IN_EDGE ** 2)
-                + state[4] * self.NB_CARNB_IN_EDGE
+        return (state[0] * 2 * self.NB_DEST_EDGE * (self.NB_POP_IN_EDGE ** 3) 
+                + state[1] * self.NB_DEST_EDGE * (self.NB_POP_IN_EDGE ** 3)
+                + state[2] * (self.NB_POP_IN_EDGE ** 3) 
+                + state[3] * (self.NB_POP_IN_EDGE ** 2)
+                + state[4] * self.NB_POP_IN_EDGE
                 + state[5])
 
-    def update_q_value(self, state, action, reward):
-        """ Updates a given Q value with the given reward. """
-        self.qtable[self.get_state_id(state), action] += reward
+    def update_q_value(self, state, action, new_q):
+        """ Updates a given Q value. """
+        self.qtable[self.get_state_id(state), action] += new_q
 
     def exploration(self):
         """ Returns a random choice of action. """
@@ -80,31 +90,42 @@ class QLearningUnit:
         else:
             return np.argmax(self.qtable[state_id])
 
-    def choose_action(self, state, possible_actions):
+    def choose_action(self, state):
         action = -1
+
+        use_state = (state[0], state[1], state[2], discr_pop(state[3]), 
+                     discr_pop(state[4]), discr_pop(state[5]))
 
         while True:
             if rd.random() > self.eps:
                 # Do exploitation
-                action = self.exploitation(state)
+                action = self.exploitation(use_state)
             else:
                 # Do exploration
                 action = self.exploration()
 
             # Check if the action is right
-            if action in possible_actions:
-                break
-            else:
+            if state[3 + action] == -1:
                 # Give very bad reward to impossible action and get new action
-                self.update_q_value(state, action, -1000)
-        
-        self.actions
+                self.update_q_value(use_state, action, -1000)
+            else:
+                break
 
         # Decay exploration rate
         if self.eps > self.min_eps:
             self.eps -= self.decay_eps
 
+        return action
 
-ql = QLearningUnit()
+    def compute_bellman(self, action, reward, curr_state, new_state):
+        """ Update a Q value using Bellman's equation. """
+        # Get the usable states
+        use_curr_state = (curr_state[0], curr_state[1], curr_state[2], discr_pop(curr_state[3]),
+                          discr_pop(curr_state[4]), discr_pop(curr_state[5]))
+        use_new_state = (new_state[0], new_state[1], new_state[2], discr_pop(new_state[3]),
+                     discr_pop(new_state[4]), discr_pop(new_state[5]))
 
-print(ql.get_state_id((1, 1, 0, 0, 0, 0)))
+        # Compute Bellman's equation and update the Q value
+        new_q = self.lr * (reward + self.dr * np.amax(self.qtable[self.get_state_id(use_new_state)]) 
+                 - self.qtable[self.get_state_id(use_curr_state), action])
+        self.update_q_value(use_curr_state, action, new_q)
